dd9af85f9301161ca7a49edcb68b6efdfa6a2930
Veenda Putri Divo <divoveenda@gmail.com>
2026-03-01 10:39:25 +0700

    Update Graph.java

--- src/Graph.java
+++ src/Graph.java
@@ -1,28 +1,33 @@
-import java.util.*;
+import java.util.HashMap;
 
 public class Graph {
     // metadata for the graph
     private Map<String, Channel> nodes = new HashMap<>(); // channels
 
     // structure of the graph
-    private Map<String, Set<String>> edges = new HashMap<>(); // channel id -> set of channel ids it is connected to
+    private Map<String, List<String>> adjList = new HashMap<>(); // channel id -> set of channel ids it is connected to
 
+    // methods to manipulate the graph
     public void addNode(String id, Channel channel) {
-        nodes.put(channel.getId(), channel);
-        edges.putIfAbsent(channel.getId(), new HashSet<>());
+        nodes.put(id, channel);
+        adjList.putIfAbsent(id, new ArrayList<>());
     }
 
+    // add an edge from one channel to another
     public void addEdge(String fromId, String toId) {
-        if (nodes.containsKey(fromId) && nodes.containsKey(toId)) {
-            edges.get(fromId).add(toId);
+        if (adjList.containsKey(fromId)) {
+            adjList.get(fromId).add(toId);
         }
     }
 
     public void printGraph() {
-        for (String id : nodes.keySet()) {
-            Channel sourceChannel = nodes.get(id);
-            System.out.println(nodes.get(id));
-            System.out.println("  Connected to: " + edges.get(id));
+        for (String fromId : adjList.keySet()) {
+            Channel from = nodes.get(fromId);
+            System.out.println(from + " is connected to:");
+            for (String toId : adjList.get(fromId)) {
+                System.out.println(nodes.get(toId) + ", ");
+            }
+            System.out.println();
         }
     }
 }
