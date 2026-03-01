5e8067081ea702d238dbe361336e9306f7b59449
Veenda Putri Divo <divoveenda@gmail.com>
2026-03-01 10:31:05 +0700

    Update Channel.java

--- src/Channel.java
+++ src/Channel.java
@@ -1,15 +1,11 @@
 public class Channel {
     private String id;
     private String name;
-    private String description;
-    private String type;
     private String link;
 
     public Channel(String id, String name, String description, String type, String link) {
         this.id = id;
         this.name = name;
-        this.description = description;
-        this.type = type;
         this.link = link;
     }
 
@@ -21,26 +17,16 @@ public class Channel {
         return name;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public String getType() {
-        return type;
-    }
-
     public String getLink() {
         return link;
     }
 
     @Override
     public String toString() {
-        return "Channel{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", type='" + type + '\'' +
-                ", link='" + link + '\'' +
-                '}';
+        return "Node_" + this.id;
+    }
+
+    public String getId() {
+        return id;
     }
 }
